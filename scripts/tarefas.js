09ea22055237fc1b61c100b9befa96dc6986f6f0
filiperslima <frodrigues144@gmail.com>
2022-09-03 18:55:50 -0300

    Adicionar tarefas Funcionando

---
+++ scripts/tarefas.js
@@ -0,0 +1,55 @@
+
+window.onload = () => {
+
+let divTasks = document.querySelector(".divTasks");
+let novaTarefa = document.getElementById("novaTarefa")
+let button = document.querySelector("button")
+const taskData = []
+
+button.addEventListener('click', addButtonEvents)
+
+function addButtonEvents(event) {
+    saveTaskData(novaTarefa.value, 'timestamp')
+    createTasks(novaTarefa.value, 'timestamp')
+    event.preventDefault()
+}
+function reloadTasks(){
+    if (localStorage.getItem("taskData")) {
+        let localTaskData = JSON.parse(localStorage.getItem("taskData"))
+        localTaskData.forEach(element => {
+            taskData.push(element)
+            createTasks(element.name, element.at)
+        });
+    }
+    else {
+        console.log("sem elemento para puxar");
+    }
+}
+function createTasks(name, timestamp) {
+    let liElement = document.createElement("li")
+    liElement.classList.add("tarefa")
+    liElement.innerHTML =
+    `
+        <div class="not-done"></div>
+        <div class="descricao">
+          <p class="nome">${name}</p>
+          <p class="timestamp">Criada em: ${timestamp}</p>
+        </div>
+      `
+    divTasks.append(liElement)
+    document.forms[0].reset()
+}
+
+function saveTaskData(value, date ) {
+    let newTask = {
+        name: `${value}`,
+        at: `${date}` 
+    }
+    taskData.push(newTask)
+    localStorage.setItem("taskData", JSON.stringify(taskData))
+}
+reloadTasks()
+
+}
+
+
