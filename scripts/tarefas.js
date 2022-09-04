e8e147079146527c256d46443ab07b9d3af79de6
filiperslima <frodrigues144@gmail.com>
2022-09-04 02:51:06 -0300

    adicionei um loadingTasks

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,55 +1,53 @@
 
 window.onload = () => {
-
-let divTasks = document.querySelector(".divTasks");
-let novaTarefa = document.getElementById("novaTarefa")
-let button = document.querySelector("button")
-const taskData = []
-
-button.addEventListener('click', addButtonEvents)
-
-function addButtonEvents(event) {
-    saveTaskData(novaTarefa.value, 'timestamp')
-    createTasks(novaTarefa.value, 'timestamp')
-    event.preventDefault()
-}
-function reloadTasks(){
-    if (localStorage.getItem("taskData")) {
-        let localTaskData = JSON.parse(localStorage.getItem("taskData"))
-        localTaskData.forEach(element => {
-            taskData.push(element)
-            createTasks(element.name, element.at)
-        });
+    let divTasks = document.querySelector(".divTasks");
+    let novaTarefa = document.getElementById("novaTarefa")
+    let button = document.querySelector("button")
+    const taskData = []
+    function loadingTasks() {
+        setTimeout(() => { divTasks.removeAttribute('id', 'skeleton') }, 700)
     }
-    else {
-        console.log("sem elemento para puxar");
+    loadingTasks()
+    button.addEventListener('click', addButtonEvents)
+    function addButtonEvents(event) {
+        saveTaskData(novaTarefa.value, 'timestamp')
+        createTasks(novaTarefa.value, 'timestamp')
+        event.preventDefault()
     }
-}
-function createTasks(name, timestamp) {
-    let liElement = document.createElement("li")
-    liElement.classList.add("tarefa")
-    liElement.innerHTML =
-    `
+    function reloadTasks() {
+        if (localStorage.getItem("taskData")) {
+            let localTaskData = JSON.parse(localStorage.getItem("taskData"))
+            localTaskData.forEach(element => {
+                taskData.push(element)
+                createTasks(element.name, element.date)
+            });
+        }
+        else {
+            console.log("sem elemento para puxar");
+        }
+    }
+    reloadTasks()
+    function createTasks(name, timestamp) {
+        let liElement = document.createElement("li")
+        liElement.classList.add("tarefa")
+        liElement.innerHTML =
+            `
         <div class="not-done"></div>
         <div class="descricao">
           <p class="nome">${name}</p>
           <p class="timestamp">Criada em: ${timestamp}</p>
         </div>
       `
-    divTasks.append(liElement)
-    document.forms[0].reset()
-}
-
-function saveTaskData(value, date ) {
-    let newTask = {
-        name: `${value}`,
-        at: `${date}` 
+        divTasks.append(liElement)
+        document.forms[0].reset()
     }
-    taskData.push(newTask)
-    localStorage.setItem("taskData", JSON.stringify(taskData))
+
+    function saveTaskData(value, date) {
+        let newTask = {
+            name: `${value}`,
+            date: `${date}`
+        }
+        taskData.push(newTask)
+        localStorage.setItem("taskData", JSON.stringify(taskData))
+    }
 }
-reloadTasks()
-
-}
-
-
