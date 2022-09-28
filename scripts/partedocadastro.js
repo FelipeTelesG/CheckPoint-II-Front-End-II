c66bf3ed095973d7be64085b5c03aa78b8662e58
filiperslima <frodrigues144@gmail.com>
2022-09-28 19:31:49 -0300

    Atualização da validação do cadastro

--- scripts/partedocadastro.js
+++ scripts/partedocadastro.js
@@ -14,7 +14,7 @@ const objeto = {};
 const listaCadastro = [];
 
 botao.addEventListener("click", (evento) => {
-      exibeSpinner()
+    exibeSpinner()
 
     evento.preventDefault();
 
@@ -89,10 +89,16 @@ senha.addEventListener("blur", (evento) => {
 
 
     if (senhaCapturado) {
-
+        setSuccessFor(senha)
         liberaBotao[3] = true;
+
     }
-    
+    else{
+        liberaBotao[3] = false;
+        setErrorFor(evento.currentTarget, "A senha deve possuir no mínimo 7 caracteres, com um ou mais caractere especial e número!")
+    }
+
+
     console.log("REPETIR_SENHA", objeto.repetirsenha)
 
     if (liberaBotao.every((item) => item)) botao.removeAttribute("disabled");
@@ -101,7 +107,7 @@ senha.addEventListener("blur", (evento) => {
     console.log("senha", liberaBotao);
 });
 
-repetirsenha.addEventListener("blur", (evento) => {
+repetirsenha.addEventListener("keyup", (evento) => {
     objeto.repetirsenha = repetirsenha.value;
     const repetirSenhaCapturado = Verificarsenha(repetirsenha.value);
 
@@ -109,16 +115,17 @@ repetirsenha.addEventListener("blur", (evento) => {
         objeto.repetirsenha = repetirsenha.value;
     }
 
-    if (senha.value === repetirsenha.value){
+    if (senha.value === repetirsenha.value) {
         liberaBotao[4] = true;
-        liberaBotao[3] = true;
+        // liberaBotao[3] = true;
         senha.classList.remove("invalid")
         senha.classList.add("valid")
         repetirsenha.classList.remove("invalid")
         repetirsenha.classList.add("valid")
         setSuccessFor(repetirsenha)
-    } 
-    else{
+        console.log(repetirSenhaCapturado);
+    }
+    else {
         liberaBotao[4] = false
         senha.classList.remove("valid")
         senha.classList.add("invalid")
@@ -174,17 +181,17 @@ botao.addEventListener("click", (evento) => {
             function (resultado) {
                 console.log(resultado);
 
-                setTimeout(() =>{
+                setTimeout(() => {
                     Swal.fire(
                         'Good job!',
                         'Usuario cadastrado com sucesso!',
                         'success'
-                      )
-                  }, 1000)
+                    )
+                }, 1000)
 
-                  setTimeout(() =>{
-                    location = "index.html" 
-                  }, 4000)
+                setTimeout(() => {
+                    location = "index.html"
+                }, 4000)
 
                 sessionStorage.setItem("jwt-cadastro", resultado.jwt)
             }
@@ -196,7 +203,7 @@ botao.addEventListener("click", (evento) => {
                     title: 'Oops...',
                     text: 'Usuário já cadastrado!',
                     footer: '<a href="index.html">Fazer Login</a>'
-                  })
+                })
             }
         )
 })
@@ -204,21 +211,21 @@ botao.addEventListener("click", (evento) => {
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector('small')
-  
-    small.innerText = message
-  
-    formControl.className = 'form-control error'
-  }
-  
-  function setSuccessFor(input) {
-    const formControl = input.parentElement;
-  
-    formControl.className = 'form-control success'
-  }
 
-function exibeSpinner(){
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function exibeSpinner() {
     let div = document.createElement("div");
     div.classList.add("loader")
     botao.innerText = ""
     botao.appendChild(div);
-  }
+}
